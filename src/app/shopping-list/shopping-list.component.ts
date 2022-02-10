04d5faa754c8872f37aaeb40d25a696e562ad8df
Rahul Choudhary <rahul0101rock@gmail.com>
2022-02-10 18:01:13 +0530

    Created Ingredients model and edited shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,16 @@
+import { Ingredients } from './../shared/ingredients.model';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients:Ingredients[] = [
+    new Ingredients("Eggs",4),
+    new Ingredients("Chocolate Bars",5)
+  ];
 
   constructor() { }
 
