017df2569326d0a7e3dc7da8d5e29da3be8eefd9
Rahul Choudhary <rahul0101rock@gmail.com>
2022-02-16 15:28:03 +0530

    Added ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -9,8 +10,16 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes : Recipe[] = [
-    new Recipe("Cake","This is a Cake","https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg"),
-    new Recipe("Pie","This is a pie","https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg")
+    new Recipe("Cake","This is a Cake","https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg",
+    [
+      new Ingredients("Bread",4),
+      new Ingredients("Cream",2),
+    ]),
+    new Recipe("Pie","This is a pie","https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg",
+    [
+      new Ingredients("Egg",4),
+      new Ingredients("Honey",1)
+    ])
   ];
 
   constructor() { }
