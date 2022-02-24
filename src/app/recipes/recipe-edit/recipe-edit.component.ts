3d331a1005c571bb76ef7102700b8cfc012a5e83
Rahul Choudhary <rahul0101rock@gmail.com>
2022-02-25 02:53:40 +0530

    Added Validators and fixed a bug

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { RecipeService } from './../recipe.service';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -30,10 +30,10 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onAddIngredient(){
-    (<FormArray>this.recipeForm.get("ingredients")).value.push(
+    (<FormArray>this.recipeForm.get("ingredients")).push(
       new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null,Validators.required),
+        'amount': new FormControl(null,[Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     );
   }
@@ -52,23 +52,23 @@ export class RecipeEditComponent implements OnInit {
       if(recipe['ingredients']){
         for(let ing of recipe.ingredients){
           recipeIngredients.push(new FormGroup({
-            'name': new FormControl(ing.name),
-            'amount': new FormControl(ing.amount)
+            'name': new FormControl(ing.name, Validators.required),
+            'amount': new FormControl(ing.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
           }));
         }
       }
     }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription),
-      'ingredients': new FormControl(recipeIngredients)
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription,Validators.required),
+      'ingredients': recipeIngredients
     });
   }
 
   get ingControls() {
     //console.log((this.recipeForm.get("ingredients") as FormArray).value.controls);
-    return (this.recipeForm.get("ingredients") as FormArray).value.controls;
+    return (this.recipeForm.get("ingredients") as FormArray).controls;
   }
 }
