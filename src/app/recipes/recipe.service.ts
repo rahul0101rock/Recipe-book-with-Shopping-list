987298ba14cf0107def9afe8f416fef4091801b2
Rahul Choudhary <rahul0101rock@gmail.com>
2022-03-03 15:57:06 +0530

    Fixing a bug in fetching

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,23 +8,24 @@ import { Recipe } from './recipe.model';
   providedIn: 'root'
 })
 export class RecipeService {
-  private recipes : Recipe[] = [
-    new Recipe("Cake","This is a Cake","https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg",
-    [
-      new Ingredients("Bread",4),
-      new Ingredients("Cream",2),
-    ]),
-    new Recipe("Pie","This is a pie","https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg",
-    [
-      new Ingredients("Egg",4),
-      new Ingredients("Honey",1)
-    ]),
-    new Recipe("Hamburger","This is a Hamburger","https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_960_720.jpg",
-    [
-      new Ingredients("Burger Bun",2),
-      new Ingredients("Patty",1)
-    ])
-  ];
+  // private recipes : Recipe[] = [
+  //   new Recipe("Cake","This is a Cake","https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg",
+  //   [
+  //     new Ingredients("Bread",4),
+  //     new Ingredients("Cream",2),
+  //   ]),
+  //   new Recipe("Pie","This is a pie","https://cdn.pixabay.com/photo/2018/08/30/20/47/gugelhupf-3643259_960_720.jpg",
+  //   [
+  //     new Ingredients("Egg",4),
+  //     new Ingredients("Honey",1)
+  //   ]),
+  //   new Recipe("Hamburger","This is a Hamburger","https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_960_720.jpg",
+  //   [
+  //     new Ingredients("Burger Bun",2),
+  //     new Ingredients("Patty",1)
+  //   ])
+  // ];
+  private recipes : Recipe[] = [];
 
   recipesChanged = new Subject<Recipe[]>(); 
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +13,20 @@ export class DataStorageService {
 
   storeRecipes(){
     const recipes = this.recipeService.getRecipes();
-    return this.http.put("https://recipe-book-rahul-default-rtdb.firebaseio.com/recipes.json",recipes).subscribe(
-      response => {
-        console.log(response);
-      }
-    );
+    return this.http.put("https://recipe-book-rahul-default-rtdb.firebaseio.com/recipes.json",recipes).subscribe();
   }
 
   fetchRecipes(){
-    this.http.get<Recipe[]>("https://recipe-book-rahul-default-rtdb.firebaseio.com/recipes.json").subscribe(
+    this.http.get<Recipe[]>("https://recipe-book-rahul-default-rtdb.firebaseio.com/recipes.json")
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe =>{
+          return {...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []}
+        });
+      })
+    )
+    .subscribe(
       recipes => {
       this.recipeService.setRecipes(recipes);
       console.log(recipes);
