d05fe0f166e810c561e456b4b6486cc1a65d911c
Rahul Choudhary <rahul0101rock@gmail.com>
2022-03-05 19:05:38 +0530

    initialized firebase and created auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,29 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthComponent } from './auth/auth.component';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjsZGhlPdhD2KztbS6YvPb3dK8f1PGE1s",
+  authDomain: "recipe-book-rahul.firebaseapp.com",
+  databaseURL: "https://recipe-book-rahul-default-rtdb.firebaseio.com",
+  projectId: "recipe-book-rahul",
+  storageBucket: "recipe-book-rahul.appspot.com",
+  messagingSenderId: "709743792443",
+  appId: "1:709743792443:web:161aeb6b23b115640ce71e",
+  measurementId: "G-VMNNBZ2WWK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 @NgModule({
   declarations: [
     AppComponent,
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import * as auth from 'firebase/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login(email: string, password: string) {
+    return auth.signInWithEmailAndPassword(auth.getAuth(), email, password);
+  }
+
+  signup(email: string, password: string, first_name: string, last_name: string) {
+      auth.createUserWithEmailAndPassword(auth.getAuth(), email, password);
+  }
+
+}
